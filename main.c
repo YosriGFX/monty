8e0a56eb0327bda32fc9b7875148665e72291719
YosriGFX <yosrig1997@gmail.com>
2020-01-04 13:08:41 -0500

    starting

---
+++ main.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+/**
+ * main - Monty
+ * @argc: int
+ * @argv: str
+ * Return: EXIT_SUCCESS on success or EXIT_FAILURE on failure
+ */
+int main(int argc, char *argv[])
+{
+
+exit(EXIT_SUCCESS);
+}
